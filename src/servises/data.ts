2431232839f6c77caedc22cd74dc6a01f2a9a6f8
Vlada Cherepanova <vlada.hero@inbox.ru>
2022-12-21 18:43:48 +0500

    added static

--- src/servises/data.ts
+++ src/servises/data.ts
@@ -63,7 +63,7 @@ export default class MovieDB {
     return `${imageBody.base_url}w500`;
   }
 
-  async getGenres() {
+  static async getGenres() {
     const genresResponse = await fetch(`${genreUrl}${apiKey}&language=en-US`);
 
     if (!genresResponse.ok) {
